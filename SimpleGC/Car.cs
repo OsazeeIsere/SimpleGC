252e5fc42562162fd667b9bac3c16edadda88f36
Osazee Isere <oz1.concepts@gmail.com>
2021-07-28 16:42:34 +0100

    initial commit

---
+++ SimpleGC/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    class Car
+    {
+        public string PetName { get; set; }
+        //the state of the car
+        public int CurrentSpeed { get; set; }
+        //a default custom constructor
+        public Car()
+        { }
+
+        //here the caller is allowed to set the full state of the Car
+        public Car(string pn,int cs)
+        {
+            PetName = pn;
+            CurrentSpeed = cs;
+        }
+        public override string ToString() => $"{PetName} is going {CurrentSpeed}";
+
+        
+    }
+
+}
--- SimpleGC/Program.cs
+++ SimpleGC/Program.cs
@@ -10,6 +10,26 @@ namespace SimpleGC
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("GC basic");
+
+            //create a new car object on the managed heap.
+            //we are returned a reference to this object
+            //("refToMyCar").
+            Car refToMyCar = new Car("Tiger Head",70);
+
+            //the c# (.) operator is used to invoke members on the object
+            //using our reference variable
+            Console.WriteLine(refToMyCar.ToString());
+
+            //print out the generation of the refToMyCar
+            Console.WriteLine("Generation of the refToMyCar is {0}",GC.GetGeneration(refToMyCar));
+            //print out the total generation
+            Console.WriteLine("this os has {0} object generation",GC.MaxGeneration+1);
+
+            //prints out the estimated number of byte on heap
+            Console.WriteLine("Estimated byte on heap: {0}",GC.GetTotalMemory(false));
+            Console.ReadLine();
+
         }
     }
 }
